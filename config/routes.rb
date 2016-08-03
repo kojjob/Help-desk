686b8dafc3303b84a69a6ab6404d88dec2b2c0c9
“Kojo <kodeon6@gmail.com>
2016-08-03 05:14:16 +0100

    Add navigation to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/features'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
   resources :invoices
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
